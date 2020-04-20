3c51542cce149dd791a58b28cd4d85ae9c9bd993
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2020-04-20 18:21:15 -0500

    Update corazon.c

--- corazon.c
+++ corazon.c
@@ -1,25 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+void tresCorazones ();
+void dosCorazones();
+void unCorazon();
 int main() {
-   int a, b, line = 4;
-   for (a = line/2; a <= line; a = a+2) { //Para la parte superior del corazon
-      for (b = 1; b < line-a; b = b+2) //crear espacio antes de la primera prominencia
-         printf(" ");
-      for (b = 1; b <= a; b++) //imprimir la primera prominencia
-         printf("*");
-      for (b = 1; b <= line-a; b++) //crear espacio antes de la segunda prominencia
-         printf(" ");
-      for (b = 1; b <= a-1; b++) //imprimir la segunda prominencia
-         printf("*");
-      printf("\n");
-   }
-   for (a = line; a >= 0; a--) { //la base del corazon es un triangulo invertido
-      for (b = a; b < line; b++) //generar espacio antes del triangulo
-         printf(" ");
-      for (b = 1; b <= ((a * 2) - 1); b++) //imprimir el triangulo
-         printf("*");
-      printf("\n");
-   }
+   tresCorazones();
+   dosCorazones();
+   unCorazon();
    return 0;
+}
+void tresCorazones(){
+	int a, b, line = 4;
+   for (a = line/2; a <= line; a = a+2) { 
+     	printf("\t");
+      for (b = 1; b < line-a; b = b+2) 
+         printf(" ");
+      for (b = 1; b <= a; b++) 
+         printf("*");
+      for (b = 1; b <= line-a; b++)
+         printf(" ");
+      for (b = 1; b <= a-1; b++) 
+         printf("*");
+         printf("\t    ");
+      for (b = 1; b < line-a; b = b+2) 
+         printf(" ");
+      for (b = 1; b <= a; b++)
+         printf("*");
+      for (b = 1; b <= line-a; b++) 
+         printf(" ");
+      for (b = 1; b <= a-1; b++) 
+         printf("*");
+         printf("\t");
+      for (b = 1; b < line-a; b = b+2) 
+         printf(" ");
+      for (b = 1; b <= a; b++)
+         printf("*");
+      for (b = 1; b <= line-a; b++) 
+         printf(" ");
+      for (b = 1; b <= a-1; b++) 
+         printf("*");
+         printf("\n");
+   }
+   for (a = line; a >= 0; a--) { 
+   	  printf("\t");
+      for (b = a; b < line; b++) 
+         printf(" ");
+      for (b = 1; b <= ((a * 2) - 1); b++) 
+         printf("*");
+      printf("\t    ");
+         for (b = a; b < line; b++) 
+         printf(" ");
+      for (b = 1; b <= ((a * 2) - 1); b++) 
+         printf("*");
+         printf("\t");
+         for (b = a; b < line; b++) 
+         printf(" ");
+      for (b = 1; b <= ((a * 2) - 1); b++) 
+         printf("*");
+         printf("\n");
+        
+   }
+}
+void dosCorazones(){
+	int a, b, line = 4;
+   for (a = line/2; a <= line; a = a+2) { 
+     	printf("\t");
+      for (b = 1; b < line-a; b = b+2) 
+         printf(" ");
+      for (b = 1; b <= a; b++) 
+         printf("*");
+      for (b = 1; b <= line-a; b++)
+         printf(" ");
+      for (b = 1; b <= a-1; b++) 
+         printf("*");
+         printf("\t    ");
+      for (b = 1; b < line-a; b = b+2) 
+         printf(" ");
+      for (b = 1; b <= a; b++)
+         printf("*");
+      for (b = 1; b <= line-a; b++) 
+         printf(" ");
+      for (b = 1; b <= a-1; b++) 
+         printf("*");
+         printf("\n");
+   }
+   for (a = line; a >= 0; a--) { 
+   	  printf("\t");
+      for (b = a; b < line; b++) 
+         printf(" ");
+      for (b = 1; b <= ((a * 2) - 1); b++) 
+         printf("*");
+      printf("\t    ");
+         for (b = a; b < line; b++) 
+         printf(" ");
+      for (b = 1; b <= ((a * 2) - 1); b++) 
+         printf("*");
+         printf("\n");
+   }
+}
+void unCorazon(){
+	int a, b, line = 4;
+   for (a = line/2; a <= line; a = a+2) { 
+     	printf("\t");
+      for (b = 1; b < line-a; b = b+2) 
+         printf(" ");
+      for (b = 1; b <= a; b++) 
+         printf("*");
+      for (b = 1; b <= line-a; b++)
+         printf(" ");
+      for (b = 1; b <= a-1; b++) 
+         printf("*");
+         printf("\n");
+   }
+    for (a = line; a >= 0; a--) { 
+   	  printf("\t");
+      for (b = a; b < line; b++) 
+         printf(" ");
+      for (b = 1; b <= ((a * 2) - 1); b++) 
+         printf("*");
+      printf("\t    ");
+         printf("\n");
+   }
 }
