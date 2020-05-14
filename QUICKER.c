88cee0937b11ff62bf7736ef8152a19041ec9eba
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2020-05-14 12:41:11 -0500

    Update QUICKER.c

--- QUICKER.c
+++ QUICKER.c
@@ -6,6 +6,7 @@ Version 1.0
 #include<conio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<windows.h>
 void titulo ();
 //selec
 void selec();
@@ -25,6 +26,18 @@ void imprimirTabla(char tabla[3][3]);
 void introducirFicha(char tabla[3][3]);
 void introducirIA(char tabla[3][3]);
 int ganador(char tabla[3][3]);
+//asteroid
+int asteroid ();
+void borde (char campo[25][60]);
+int bajar (char campo[25][60]);
+void nave(char campo[25][60],int navex, int navey);
+void meteorito(char campo[25][60]);
+void imprimir(char campo[25][60]);
+void draw(char campo[25][60]);
+int gameloop(char campo[25][60],int navex, int navey);
+void input(char campo[25][60],int *navex, int *navey);
+//pong
+
 //gameover
 void gameOver(int puntos);
 int main (){
@@ -82,11 +95,12 @@ void selec (){
 		sleep(2);
 		system("cls");
 		srand(time(NULL));
-		juego = rand()%2; //cambiar dependiendo del numero de juegos
+		juego = rand()%4; //cambiar dependiendo del numero de juegos
 		switch (juego) {
 			case 0: resultado(preguntas(acierto),&acierto,&vida); break;
 			case 1: resultado(gato(),&acierto,&vida); break;
-			case 2: break;
+			case 2: resultado(asteroid(),&acierto,&vida);break;
+			//case 3: resultado(pong(),&acierto,&vida);break;
 		}
 	}
 	gameOver(acierto);
@@ -641,6 +655,168 @@ int ganador (char tabla[3][3]){
 	}
 	return 2; 
 }
+//asteroid
+int asteroid(){
+	char campo[25][60];
+	int navex=22,navey=30, resultado;
+	borde(campo);
+	nave(campo,navex,navey);
+	imprimir(campo);
+	resultado=gameloop(campo,navex,navey);
+	return resultado;
+}
+void borde (char campo[25][60]){
+	int i,j,V=25,H=60;
+	for(i=0;i<V;i++){
+		for (j=0;j!=H;j++){
+			if(i==0||i==V-1){
+				campo[i][j]='_';
+			}else if(j==0||j==H-1){
+				campo[i][j]='|';
+			}else{
+				campo[i][j]=' ';
+			}
+		}
+	}
+	return;
+}
+void nave (char campo[25][60],int navex, int navey){
+	campo[navex][navey]='X';
+	campo[navex+1][navey-1]='X';
+	campo[navex+1][navey]='X';
+	campo[navex+1][navey+1]='X';
+	return;
+}
+void imprimir(char campo[25][60]){
+		int i,j,V=25,H=60;
+		for(i=0;i<V;i++){
+		for (j=0;j!=H;j++){
+		printf("%c",campo[i][j]);
+		}
+		printf("\n");
+	}
+	return;
+}
+int gameloop(char campo[25][60],int navex, int navey){
+	int lose=0, d=1;
+	int resu=0;
+	time_t tiempoInicial=time(NULL);
+	float tiempoLimite=10;
+	do{
+	draw(campo);
+	if(d==1){
+	meteorito(campo);
+	d=0;
+}
+	lose=bajar(campo);
+	d++;
+	input(campo,&navex,&navey);
+	input(campo,&navex,&navey);
+	input(campo,&navex,&navey);
+	input(campo,&navex,&navey);
+	input(campo,&navex,&navey);
+	input(campo,&navex,&navey);
+	input(campo,&navex,&navey);
+	input(campo,&navex,&navey);
+	input(campo,&navex,&navey);
+	//update();
+	}while(lose==0&&(time(NULL)-tiempoInicial)<tiempoLimite);
+	if (lose==0) {
+		resu=1;
+	}
+	sleep(1);
+	system ("cls");
+	return resu;
+}
+void draw(char campo[25][60]){
+	system("cls");
+	imprimir(campo);
+}
+void meteorito(char campo[25][60]){
+	int num,a;
+	num=2+rand()%(57-2);
+	campo[2][num]='@';
+	campo[2][num+1]='@';
+	campo[2][num-1]='@';
+	campo[1][num]='@';
+	campo[3][num]='@';
+
+return;
+}
+int bajar (char campo[25][60]){
+	int i,j,V=25,H=60,w=0;
+	for(i=23;i>1;i--){
+		for(j=H-1;j>1;j--){
+			if(campo[i][j]!='X'){
+			if(campo[i-1][j]!='X'){
+			campo[i][j]=campo[i-1][j];
+			campo[i-1][j]=' ';
+		}
+		}else{
+			if(campo[i-1][j]=='@'){
+				w=1;
+			}
+		}
+		}
+	}
+return w;
+}
+void input(char campo[25][60],int *navex, int *navey){
+	//parte de verificar
+	char key;
+	if (kbhit()==1){
+		key=getch();
+		if(key=='a'||key=='A'){
+			if((*navey-1)!=1){
+				campo[*navex][*navey-1]='X';
+				campo[*navex][*navey]=' ';
+				campo[*navex+1][*navey-2]='X';
+	            campo[*navex+1][*navey-1]='X';
+	            campo[*navex+1][*navey]='X';
+	            campo[*navex+1][*navey+1]=' ';
+				*navey=*navey-1;	
+			}
+		}
+		if(key=='d'||key=='D'){
+			if((*navey+1)!=59){
+				campo[*navex][*navey+1]='X';
+				campo[*navex][*navey]=' ';
+				campo[*navex+1][*navey+2]='X';
+	            campo[*navex+1][*navey+1]='X';
+	            campo[*navex+1][*navey]='X';
+	            campo[*navex+1][*navey-1]=' ';
+				*navey=*navey+1;	
+			}
+		}
+			if(key=='s'||key=='S'){
+			if((*navex+1)!=24){
+				campo[*navex+2][*navey]='X';
+				campo[*navex+1][*navey]='X';
+				campo[*navex+2][*navey+1]='X';
+				campo[*navex+2][*navey-1]='X';
+	            campo[*navex][*navey]=' ';
+	            campo[*navex+1][*navey-1]=' ';
+	            campo[*navex+1][*navey+1]=' ';
+				*navex=*navex+1;	
+			}
+		}
+		    if(key=='W'||key=='w'){
+			if((*navex-1)!=7){
+				campo[*navex-1][*navey]='X';
+				campo[*navex][*navey]='X';
+				campo[*navex][*navey+1]='X';
+				campo[*navex][*navey-1]='X';
+	            campo[*navex+1][*navey]=' ';
+	            campo[*navex+1][*navey-1]=' ';
+	            campo[*navex+1][*navey+1]=' ';
+				*navex=*navex-1;	
+			}
+		}
+	}
+return;	
+}
+//pong
+
 //gameover
 void gameOver (int puntos){
 	puts("\n\n\t #####");
@@ -649,6 +825,6 @@ void gameOver (int puntos){
 	puts("\t##   ## ##   ##  ##   ## ##       #   #   ## ##    ##");
 	puts("\t #####   ####### ##   ## #####    #####    # ##### ##");
 	printf("\n\t\t\t   PUNTAJE:   %d\n\n\n\t",puntos);
-	system("pause");
+	sleep("4");
 	system("cls");
 }
