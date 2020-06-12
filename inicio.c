6ac5cc00e4ce14fc5b719891dc6a95a0da88155d
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2020-06-11 20:44:54 -0500

    Update inicio.c

--- inicio.c
+++ inicio.c
@@ -2,84 +2,47 @@
 #include<conio.h>
 #include<stdlib.h>
 void titulo ();
+int opcionesMenu(int i);
 int main (){
-	char tecla=1;
-	int i;
-	while (tecla!=0){
+	char tecla;
+	int seleccion, i=0;
+	while (tecla!=10){
 		system("color 02");
 		titulo ();
-		puts("\t\t\tSelecciona:");
-		printf("\t\t\t->"); puts(" Empezar");
-		printf("\t\t\t  "); puts(" Puntuaciones");
-		printf("\t\t\t  "); puts(" Salir");
-		puts("\n\t\t\t\t\t\tWCORP. 2020.");
+		seleccion=opcionesMenu(i);
 		tecla=getch();
+		if (tecla=='s'||tecla=='S') {
+			i++;
+		}
+		if (tecla=='w'||tecla=='W'){
+			i--;
+		}
+		if(tecla==13){
+			if (seleccion==0){
+				puts("comenzar");//colocar funcion
+				system ("pause");
+			}
+			else {
+				if (seleccion==1){
+					puts("puntuaciones");
+					system ("pause");
+				}
+				else {
+					tecla=10;
+				}
+			}
+		}
+		if (i<0){
+			i=0;
+		}
+		if (i>2){
+			i=2;
+		}
 		system("cls");
-		if (tecla=='s'||tecla=='S') {
-			titulo();
-			puts("\t\t\tSelecciona:");
-			printf("\t\t\t  "); puts(" Empezar");
-			printf("\t\t\t->"); puts(" Puntuaciones");
-			printf("\t\t\t  "); puts(" Salir");
-			puts("\n\t\t\t\t\t\tWCORP. 2020.");
-			tecla=getch();
-			system("cls");
-			if (tecla==13){
-					puts("puntuacion");
-					system("pause");
-					system("cls");
-					tecla=1;
-			}
-			if (tecla=='w'||tecla=='W'){
-				continue;
-			}
-		}
-		if (tecla=='s'||tecla=='S') {
-			titulo();
-			puts("\t\t\tSelecciona:");
-			printf("\t\t\t  "); puts(" Empezar");
-			printf("\t\t\t  "); puts(" Puntuaciones");
-			printf("\t\t\t->"); puts(" Salir");
-			puts("\n\t\t\t\t\t\tWCORP. 2020.");
-			tecla=getch();
-			system("cls");
-			if (tecla==13){
-				break;
-			}
-			if (tecla=='w'||tecla=='W'){
-				titulo();
-				puts("\t\t\tSelecciona:");
-				printf("\t\t\t  "); puts(" Empezar");
-				printf("\t\t\t->"); puts(" Puntuaciones");
-				printf("\t\t\t  "); puts(" Salir");
-				puts("\n\t\t\t\t\t\tWCORP. 2020.");
-				tecla=getch();
-				system("cls");
-				if (tecla==13){
-					puts("puntuacion");
-					system("pause");
-					system("cls");
-					tecla=1;
-					//puntuacion;
-				}
-				if (tecla=='w'||tecla=='W'){
-					continue;
-				}
-			}
-		}
-		if (tecla==13) {
-			//pantalla vida puntos en 0 y comienza partida
-			system("cls");
-			puts("hola");
-			/*system("cls");
-			selec();
-			system("cls");*/
-			system("pause");//prueba
-			system("cls");
-		}
 	}
 	return 0;
 }
+	
 void titulo (){
 	puts("\n");
 	puts("\t===================================================\n");
@@ -91,4 +54,24 @@ void titulo (){
 	puts("\t   #####    ######  ##  #####  ##  ##  ####  ##");
 	puts("\t        #   ");
 	puts("\t===================================================\n");
+}
+int opcionesMenu(int i){
+	switch (i){
+		case 0: puts("\t\t\tSelecciona:");
+				puts("\t\t\t-> Empezar"); 
+				puts("\t\t\t   Puntuaciones"); 
+				puts("\t\t\t   Salir"); 
+				puts("\n\t\t\t\t\t\tWCORP. 2020."); return 0;
+		case 1: puts("\t\t\tSelecciona:");
+				puts("\t\t\t   Empezar"); 
+				puts("\t\t\t-> Puntuaciones"); 
+				puts("\t\t\t   Salir"); 
+				puts("\n\t\t\t\t\t\tWCORP. 2020."); return 1;
+		case 2: puts("\t\t\tSelecciona:");
+				puts("\t\t\t   Empezar"); 
+				puts("\t\t\t   Puntuaciones"); 
+				puts("\t\t\t-> Salir"); 
+				puts("\n\t\t\t\t\t\tWCORP. 2020."); return 2;
+	}
+	return 0;
 }
